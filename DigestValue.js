5c6ab1b02e8ef60834b3bf7ed6b9fe60bef286c4
Nejc Vukovic <nejc.vukovic@gmail.com>
2016-07-15 01:21:45 +0200

    Loading hash-values to the digest-value

--- DigestValue.js
+++ DigestValue.js
@@ -6,19 +6,51 @@
 */
 
 const validators = true;
-let URLs = [[url, ETag] = ['https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css', 'W/"385b964b68acb68d23cb43a5218fade9"'], [url, ETag] = ['https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js', 'W/"abda843684d022f3bc22bc83927fe05f"']];
+const P = 32; // 2 power of 5
 
+let url;
+let etag;
+
+let URLs = [
+  [url, etag] = ['http://localhost:1337/test.js', 'W/"385b964b68acb68d23cb43a5218fade9"'],
+  [url, etag] = ['https://localhost:443/testFolder/main.css', 'W/"385b964b68a5248d23cb43a5218fade9"'],
+  [url, etag] = ['https://code.jquery.com/jquery-3.1.0.js', 'W/"385b964b68acb68d23cb43a5218adef9"'],
+  [url, etag] = ['https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/4/bootstrap.min.css']
+];
 
 function DigestValue(validators, URLs, P){
-  //for tests uncomment the next lines
-  let N = this._roundToNearestTwoPower(URLs.length);
+  this.validators = validators;
+  this.URLs = URLs;
+  this.P = P;
+  this.N = this._roundToNearestTwoPower(this.URLs.length);
+  this.hash_values = this._loadHashValues(this.URLs, this.validators, this.N, this.P);
+  this.digest_value = new ArrayBuffer(this.URLs.length);
 
-  this.N = this.roundToNearestTwoPower(N);
-  this.hashValues = [0];  //integer
+  return this;
 }
 
 DigestValue.prototype._roundToNearestTwoPower = function(N){
   return Math.pow(2,Math.floor(Math.log(N)/Math.log(2)));
 };
 
-DigestValue.prototype._loadHashValues = function(URLs)
+DigestValue.prototype._loadHashValues = function(URLs, validators, N, P){
+  let hashValue;
+  let hashValues = [];
+
+  hashValues.push(0);
+
+  for (let i = 0; i < URLs.length; i++){
+    hashValue = new HashValue(URLs[i], validators, N, P);
+    hashValues.push(Number(hashValue.hash_value));
+  }
+
+  hashValues.sort();
+
+  return hashValues;
+};
+
+let testDigestsValue = new DigestValue(validators, URLs, P);
+
+console.time('test');
+console.log(testDigestsValue);
+console.timeEnd('test');
